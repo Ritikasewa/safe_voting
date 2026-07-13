0e64da4e59c994ca27b5c265ab09c4cbfc820dc1
Ritikasewa <230377951+Ritikasewa@users.noreply.github.com>
2026-07-13 21:31:33 +0530

    Update contract.js

--- src/lib/contract.js
+++ src/lib/contract.js
@@ -1,4 +1,4 @@
 import contractData from '../VotingDAO.json';
 
-export const CONTRACT_ADDRESS = "0x71EF9C0Cf92F18F59367B784681654b4dede3146";
+export const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
 export const CONTRACT_ABI = contractData.abi;
